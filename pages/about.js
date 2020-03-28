5f6cf95eb64ec6204e70a2d09876df24e18960de
celinechole <github@celinechole.com>
2020-03-28 14:34:44 -0400

    fetch using swr

--- pages/about.js
+++ pages/about.js
@@ -1,22 +1,30 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
+import useSWR from "swr";
 
 const About = () => {
-
-  const [elephantName, setElephantName] = useState('');
+  const [elephantName, setElephantName] = useState("");
 
   useEffect(() => {
     const fetchElephant = async () => {
-      const elephant = await fetch('/api/elephant')
-      const res = await elephant.json()
-      console.log(res)
-      setElephantName(res.name)
-    }
-    fetchElephant()
-  }, [])
+      const elephant = await fetch("/api/elephant");
+      const res = await elephant.json();
+      setElephantName(res.name);
+    };
+    fetchElephant();
+  }, []);
+
+  const { data, error } = useSWR("/api/elephant", (...args) => {
+    return fetch(...args).then(res => res.json());
+  });
 
   return (
-    <div>Client side fetch: {elephantName}</div>
-  )
-}
+    <>
+      <div>Elephant from SWR {data ? data.name : ""} </div>
+      {/* display error test */}
+      {error && <div>Oups, error, {error.toString()}</div>}
+      <div>Client side fetch {elephantName}</div>
+    </>
+  );
+};
 
-export default About
+export default About;
